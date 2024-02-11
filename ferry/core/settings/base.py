1bba4d1335cc6c11d2834011ff338f6d864bc36e
Dan Trickey <git@trickey.io>
2024-02-11 17:29:21 +0000

    relative directories are good

--- ferry/core/settings/base.py
+++ ferry/core/settings/base.py
@@ -40,7 +40,7 @@ ROOT_URLCONF = "ferry.core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['templates'],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
