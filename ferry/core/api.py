57b8340eebd5b2eee0c40e66aebeba3885cd319c
Dan Trickey <git@trickey.io>
2024-02-12 18:45:13 +0000

    Handle ProtectedError

--- ferry/core/api.py
+++ ferry/core/api.py
@@ -1,4 +1,5 @@
-from django.http import HttpRequest
+from django.db.models.deletion import ProtectedError
+from django.http import HttpRequest, HttpResponse
 from ninja import NinjaAPI, Swagger
 from ninja.security import HttpBearer
 
@@ -21,5 +22,15 @@ api = NinjaAPI(title="Ferry API", auth=TokenAuth(), docs=Swagger(settings={"pers
 
 api.add_router("/users", accounts_api)
 api.add_router("/", court_api)
+
+
+@api.exception_handler(ProtectedError)
+def protected_error_handler(request: HttpRequest, exc: ProtectedError) -> HttpResponse:
+    return api.create_response(
+        request,
+        {"message": "Unable to delete as referenced by other objects"},
+        status=400,
+    )
+
 
 urls = api.urls
