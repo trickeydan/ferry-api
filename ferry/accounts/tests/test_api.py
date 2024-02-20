3f4e68260dfa24b1264adff056210ef23fccee42
Dan Trickey <git@trickey.io>
2024-02-20 20:16:59 +0000

    Refactor account api tests to use APITest

--- ferry/accounts/tests/test_api.py
+++ ferry/accounts/tests/test_api.py
@@ -7,14 +7,14 @@ import pytest
 from django.test import Client
 from django.urls import reverse_lazy
 
-from ferry.conftest import GetAPIToken
+from ferry.court.tests.utils import APITest
 
 if TYPE_CHECKING:
     from ferry.accounts.models import User
 
 
 @pytest.mark.django_db
-class TestUserInfoEndpoint:
+class TestUserInfoEndpoint(APITest):
     url = reverse_lazy("api-1.0.0:users_me")
 
     def test_get_missing_auth_header(self, client: Client) -> None:
@@ -25,12 +25,9 @@ class TestUserInfoEndpoint:
         resp = client.get(self.url, headers={"Authorization": "Bearer bees"})
         assert resp.status_code == HTTPStatus.UNAUTHORIZED
 
-    def test_get(self, client: Client, user_with_person: User, get_api_token: GetAPIToken) -> None:
-        # Arrange
-        api_token = get_api_token(user_with_person)
-
+    def test_get(self, client: Client, user_with_person: User) -> None:
         # Act
-        resp = client.get(self.url, headers={"Authorization": f"Bearer {api_token.token}"})
+        resp = client.get(self.url, headers=self.get_headers(user_with_person))
 
         # Assert
         assert resp.status_code == HTTPStatus.OK
