c865fbd79deead5b6125c9efcfb27fc8b004ce0f
Dan Trickey <git@trickey.io>
2024-02-21 19:37:54 +0000

    chore: Remove unused api_token fixture

--- ferry/conftest.py
+++ ferry/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ferry.accounts.models import APIToken, User
+from ferry.accounts.models import User
 from ferry.court.factories import PersonFactory
 from ferry.court.models import Person
 
@@ -18,9 +18,3 @@ def user_with_person(person: Person) -> User:
 @pytest.fixture
 def admin_user() -> User:
     return User.objects.create(username="admin", is_superuser=True)
-
-
-@pytest.fixture
-def api_token(admin_user: User) -> APIToken:
-    # DEPRECATED
-    return admin_user.api_tokens.create(is_active=True)
