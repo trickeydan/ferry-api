75d5dcc46e0fec00e8f0de4632a6763f395a21cc
Dan Trickey <git@trickey.io>
2024-10-15 20:42:27 +0100

    Add attendance response buttons to web interface

--- ferry/pub/urls.py
+++ ferry/pub/urls.py
@@ -7,4 +7,5 @@ app_name = "pub"
 urlpatterns = [
     path("events", views.PubEventListView.as_view(), name="events-list"),
     path("events/<uuid:pk>/", views.PubEventDetailView.as_view(), name="events-detail"),
+    path("event/<uuid:pk>/update-response/", views.UpdatePubEventResponseView.as_view(), name="events-update-response"),
 ]
--- ferry/pub/views.py
+++ ferry/pub/views.py
@@ -1,11 +1,19 @@
 from typing import Any
+from uuid import UUID
 
+from django import http
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import SuspiciousOperation
+from django.db import transaction
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
+from django.utils import timezone
+from django.views import View
 from django.views.generic import DetailView, ListView
 
+from ferry.core.http import HttpRequest
 from ferry.core.mixins import BreadcrumbsMixin
-from ferry.pub.models import PubEvent, PubEventQuerySet
+from ferry.pub.models import PubEvent, PubEventQuerySet, PubEventRSVP, PubEventRSVPMethod
 from ferry.pub.repository import annotate_attendee_count, get_attendees_for_pub_event
 
 
@@ -21,9 +29,20 @@ class PubEventListView(LoginRequiredMixin, BreadcrumbsMixin, ListView):
         return qs
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        assert self.request.user.is_authenticated
+        assert self.request.user.person
+
         upcoming_pub = PubEvent.objects.get_next()
+        if upcoming_pub:
+            upcoming_pub_rsvp = PubEventRSVP.objects.filter(
+                pub_event=upcoming_pub, person=self.request.user.person
+            ).first()
+        else:
+            upcoming_pub_rsvp = None
+
         return super().get_context_data(
             upcoming_pub=upcoming_pub,
+            upcoming_pub_rsvp=upcoming_pub_rsvp,
             attendees=get_attendees_for_pub_event(upcoming_pub) if upcoming_pub else None,
             **kwargs,
         )
@@ -46,7 +65,70 @@ class PubEventDetailView(LoginRequiredMixin, BreadcrumbsMixin, DetailView):
         return qs
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        assert self.request.user.is_authenticated
+        assert self.request.user.person
+
+        rsvp = PubEventRSVP.objects.filter(pub_event=self.object, person=self.request.user.person).first()
         return super().get_context_data(
             attendees=get_attendees_for_pub_event(self.object),
+            rsvp=rsvp,
+            is_past=timezone.now().date() > self.object.timestamp.date(),
             **kwargs,
+        )
+
+
+class UpdatePubEventResponseView(LoginRequiredMixin, View):
+    def post(self, request: HttpRequest, pk: UUID) -> http.HttpResponse:
+        assert request.user.is_authenticated
+        assert request.user.person
+
+        if not request.htmx:
+            raise SuspiciousOperation("That request is not valid.")
+
+        pub_event = get_object_or_404(PubEvent.objects.for_user(request.user), id=pk)
+        is_past = timezone.now().date() > pub_event.timestamp.date()
+        rsvp = PubEventRSVP.objects.filter(
+            pub_event=pub_event,
+            person=request.user.person,
+        ).first()
+
+        if not is_past:
+            if rsvp:
+                if rsvp.method == PubEventRSVPMethod.AUTO:
+                    with transaction.atomic():
+                        rsvp.delete()
+                        rsvp = PubEventRSVP(
+                            pub_event=pub_event,
+                            person=request.user.person,
+                            is_attending=False,
+                            method=PubEventRSVPMethod.WEB,
+                        )
+                        rsvp.save()
+                elif rsvp.method == PubEventRSVPMethod.DISCORD:
+                    # No action
+                    pass
+                elif rsvp.method == PubEventRSVPMethod.WEB:
+                    rsvp.is_attending = not rsvp.is_attending
+                    rsvp.save()
+                else:
+                    # This should be unreachable
+                    pass
+            else:
+                rsvp = PubEventRSVP(
+                    pub_event=pub_event,
+                    person=request.user.person,
+                    is_attending=True,
+                    method=PubEventRSVPMethod.WEB,
+                )
+                rsvp.save()
+
+        return render(
+            request,
+            "pub/inc/event_details.html",
+            {
+                "event": pub_event,
+                "rsvp": rsvp,
+                "attendees": get_attendees_for_pub_event(pub_event),
+                "is_past": is_past,
+            },
         )
