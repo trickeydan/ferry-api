1d02c1126c259ccbc4627ad7ce181d7937472639
Dan Trickey <git@trickey.io>
2025-04-05 15:38:30 +0100

    Use granian in production instead of gunicorn

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -6,7 +6,9 @@ set -o nounset
 
 export DJANGO_SETTINGS_MODULE=ferry.core.settings.prod
 
+cd /app
+
 python /app/manage.py collectstatic --noinput
 python /app/manage.py migrate
 
-/app/.venv/bin/gunicorn ferry.core.wsgi:application --bind 0.0.0.0:8000 --chdir=/app
+/app/.venv/bin/granian --interface asginl ferry.core.asgi:application --workers 2 --no-ws --host 0.0.0.0 --port 8000
